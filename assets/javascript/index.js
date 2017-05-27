4cb444702cb5dcc61ccb63f2fc5834da6819c00d
ibarram2 <scorpiobox@hotmail.com>
2017-05-27 09:15:43 -0700

    submit

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -23,20 +23,23 @@ $(document).ready(function() {
 		var tName = $("#train-name-input").val().trim();
 		var tDest = $("#destination-input").val().trim();
 		var tFirstTime = $("#start-input").val().trim();
-		var tFreq = $("#freq-input").val().trim();
-	
+		var tFreq = $("#freq-input").val().trim(); 
+
+		//var a = moment([2007, 0, 29]);
+		
+		 // 1
+
 		var newTrain = {
     		name: tName,
 			destination: tDest,
 			time: tFirstTime,
-    		frequency: tFreq
+    		frequency: tFreq,
 		 };
 		 
 		 console.log(newTrain.name);
   		 console.log(newTrain.destination);
   		 console.log(newTrain.time);
          console.log(newTrain.frequency);
-		
 
 		//get new key
 		 database.ref().push(newTrain);
@@ -46,6 +49,7 @@ $(document).ready(function() {
 	
 	//???
 	database.ref().on("child_added", function(snapshot, prevChildKey) {
+
 		var newPost = snapshot.val();
 		console.log("Name: " + newPost.name);
 		console.log("Destination: " + newPost.destination);
@@ -53,23 +57,47 @@ $(document).ready(function() {
 		console.log("Frequency: " + newPost.frequency);
 		var newName = newPost.name;
 		var newDest = newPost.destination;
-		var newTime = newPost.time;
+		var newTime = newPost.time
 		var newFreq = newPost.frequency;
+		//var newTime = moment(newPost.time).format("mm"); //not a valid date
+		//var newFreq = moment(newPost.frequency).format("mm");
+		var tNow = moment().format();
+		var Frequency = moment(newTime._i);
+
+		//moment("not a real date").isValid();
+		//var duration = moment.duration(tNow).diff(newTime);//not a function?
+		//tNow.diff(newTime, "minutes", true);//not a function?
+		//var tRemaining = tNow.diff(newTime, "minutes", true);
+		//var tRemaining = moment.duration(tNow.diff(newTime));
+		//var time = tRemaining.asHours();
+
+		//moment(newTime).diff(moment.unix(tNow),'minutes');
+		//console.log(newFreq);
+		//console.log(tRemaining);
+		//console.log(time);
+		console.log(tNow);
+		console.log(newTime);
+
+		//console.log(newTime._i);
+		console.log(Frequency);
+
+		//console.log(newTime);
 
 		//var empMonths = moment().diff(moment.unix(newStart, "X"), "months");
 		//console.log(empMonths);
   
   		//var empBilled = empMonths * newRate;
-  		//console.log(empBilled);
-		
+  		//console.log(empBilled);		
 	
 		$("#train-table > tbody").append(
 			"<tr><td>"
 			+ newName + "</td><td>"
 			+ newDest + "</td><td>"
-			+ newTime + "</td><td>"
+			+ newFreq + "</td><td>"
 			// note return of moments is something like "35 years"
-			+ newFreq + "</td></tr>")
+			+ newTime + "</td></tr>"
+			//+ tRemaining + "</td></tr>"
+			)
 
 	});
 	
